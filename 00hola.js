9f351940c691f577f685b814dda37283957ddfba
zeqzaq <cristianarcilamarques123@gmail.com>
2025-05-20 00:35:02 -0500

    Hola mundo

---
+++ 00hola.js
@@ -0,0 +1,5 @@
+// Para ejecutar este archivo, usa el siguiente comando:
+// node 00hola.js tiene que estar en la misma carpeta que el archivo
+// o en la carpeta padre
+
+console.log("Hola Mundo");
